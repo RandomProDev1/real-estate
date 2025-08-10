efb3a2c8662bf80e52d8e860533c1cf8be54d5d7
RandomProDev1 <hastyarclash1@gmail.com>
2025-08-10 11:12:53 +0300

    added mongo db and connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://hastyar:Hawler1234@cluster0.myva3pn.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=Cluster0").then(() => {console.log("Connected to monogodb")})
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!m')
+    console.log('Server is running on port 3000!')
 })
