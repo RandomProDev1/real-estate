afdbc909572f5c2f25da9a870dae069d4ddcda96
RandomProDev1 <hastyarclash1@gmail.com>
2025-08-10 11:40:34 +0300

    Created A Test API Route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 mongoose.connect("mongodb+srv://hastyar:Hawler1234@cluster0.myva3pn.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=Cluster0").then(() => {console.log("Connected to monogodb")})
 
@@ -7,4 +8,6 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
-})
+});
+
+app.use('/api/user', userRouter)
