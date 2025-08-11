ad6d1470829ea994edfd3508ce22746c1d2e0a4a
RandomProDev1 <hastyarclash1@gmail.com>
2025-08-11 12:49:20 +0300

    Created API Sign UP Route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://hastyar:Hawler1234@cluster0.myva3pn.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=Cluster0").then(() => {console.log("Connected to monogodb")})
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
 });
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
